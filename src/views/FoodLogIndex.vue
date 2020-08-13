a6aad487400e1c3ce151db36268d91a923621ee3
chuck1193 <>
2020-08-12 22:53:26 -0500

    Food Log Show

--- src/views/FoodLogIndex.vue
+++ src/views/FoodLogIndex.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container">
+  <div class="food-logs-index">
     <h1>Logs</h1>
-    <div v-for="foodLog in foodLogs">
+    <div v:for="foodLog in foodLogs">
       <h2>{{ foodLog.food_type }}: {{foodLog.food}}</h2>
       <h3>{{ foodLog.calories }}</h3>
       <h3>{{ foodLog.water_intake}}</h3>
